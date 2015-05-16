5ebc319590dee1c90bdb3d99030701404122d1a1
Ubuntu <ubuntu@devops.(none)>
2015-05-16 21:40:27 +0000

    springform working

--- scenarios/jenkins/add_host_entry.sh
+++ scenarios/jenkins/add_host_entry.sh
@@ -7,7 +7,7 @@ fi
 sudo cp /etc/hosts  /etc/hosts.$(date -d "today" +"%Y%m%d%H%M")
 
 # Delete the entry if it exists
-sed  '/'$1'/d' -i  /etc/hosts | sudo tee --append /etc/hosts
+sed  '/'$2'/d' -i  /etc/hosts | sudo tee --append /etc/hosts
 
 # Add entry
 echo "${1}     ${2}" | sudo tee --append /etc/hosts
