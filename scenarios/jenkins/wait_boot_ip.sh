688b4de18660f9dcecf4ca5f9ce2a5b18e2cdb5a
Bob Webster <rdbwebster@gmail.com>
2015-05-21 12:26:17 -0400

    added stderr output

--- scenarios/jenkins/wait_boot_ip.sh
+++ scenarios/jenkins/wait_boot_ip.sh
@@ -1,4 +1,3 @@
-
 if [ "$#" -ne 2 ];
     then echo "Usage: wait_boot_ip  vapp_name  vm_name"
     exit
@@ -7,11 +6,11 @@ fi
 # get the new vm ip 
 for a in 1 2 3 4 5 6 7 8 9; do
      export IP=`vca vm -a $1 | grep $2 | cut -d '|' -f5 |  tr -d '[[:space:]]'`
-  
-     echo "Obtain IP attempt $a"
+
+     >&2 echo "Obtain IP attempt $a"
+
      if [ ${#IP} -gt 1 ]
      then
-       echo "New VM at $IP"
        break
      else
        sleep 10
