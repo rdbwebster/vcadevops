29f910ef05dfe27d7f5ef705335b52e9a5a06f76
Ubuntu <ubuntu@devops.(none)>
2015-05-22 20:29:24 +0000

    formtest

--- scenarios/wordFinder/selenium/formtest.py
+++ scenarios/wordFinder/selenium/formtest.py
@@ -1,8 +1,11 @@
-
 #!/usr/bin/env python
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 display = Display(visible=0, size=(800, 600))
 display.start()
@@ -11,25 +14,31 @@ display.start()
 # you will not see the browser.
 browser = webdriver.Firefox()
 # Cannot test from inside against devops.vcloudair.io not gateway support for hairpin nat (out and back in again)
-browser.get('http://linuxsb:3000/greeting')
-
- # <form action="/greeting" method="post">
- #   	<p>Id: <input type="text" id="id" name="id" value="0" /></p>
- #       <p>Message: <input type="text" id="content" name="content" value="" /></p>
- #       <p><input type="submit" value="Submit" /> <input type="reset" value="Reset" /></p>
- #   </form>
+browser.get('http://linuxa:3000/')
 
 print browser.title
-el = browser.find_element_by_name('content')
-el.send_keys('hello')
+el = browser.find_element_by_name('pattern')
+el.send_keys('he__o')
 
-submit = browser.find_element_by_xpath("//input[@value='Submit']")
-#submit = browser.find_element_by_xpath("//form[1]/input[3]")
-submit.click()
+print "Entered value " + el.get_attribute('value')
+
+# Hit enter key
+el.send_keys(Keys.RETURN)
+
+
+element = WebDriverWait(browser, 10)  #.until(
+#        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+#)
+
+#div = self.driver.find_element_by_class_name('someclass')
+#div.find_element_by_css_selector('a').get_attribute('href')
+
 browser.save_screenshot('screenshot.png')
 
-element = browser.find_element_by_css_selector('a').click()
+result = browser.find_element_by_xpath("//div[3]/p")
+print result.get_attribute('textContent')
 
+assert "Words found: 2" in browser.page_source
 browser.quit()
 
 display.stop()
