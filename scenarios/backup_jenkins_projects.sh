460e8bdd693b924cdb790227aa63cbd5947f8a8c
Bob Webster <rdbwebster@gmail.com>
2015-10-20 11:32:52 -0400

    fixed jenkins job backup script and captured job backups

--- scenarios/backup_jenkins_projects.sh
+++ scenarios/backup_jenkins_projects.sh
@@ -1,27 +1,30 @@
+cd /tmp
+wget http://vcair.us:8100/jnlpJars/jenkins-cli.jar
+cd -
 
-#cd /tmp
-#wget http://localhost:8100/jnlpJars/jenkins-cli.jar
-
-#java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ help --username jenkins --password devops123
+#java -jar mp/jenkins-cli.jar -s http://vcair.us:8100/ help --username jenkins --password devops123
 
 # create_photon
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job create_photon --username jenkins --password $PASSWORD  > /home/ubuntu/vcadevops/scenarios/createPhoton/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job create_photon --username jenkins --password $PASSWORD  > ./createPhoton/jenkins/config.xml
 
 # create_ubuntu
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job create_ubuntu --username jenkins --password $PASSWORD > /home/ubuntu/vcadevops/scenarios/createUbuntu/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job create_ubuntu --username jenkins --password $PASSWORD > ./createUbuntu/jenkins/config.xml
 
 # docker_photon
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job docker_photon --username jenkins --password $PASSWORD  > /home/ubuntu/vcadevops/scenarios/dockerPhoton/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job docker_photon --username jenkins --password $PASSWORD  > ./dockerPhoton/jenkins/config.xml
 
 # spring_boot_form
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job spring_boot_form  --username jenkins --password $PASSWORD  > /home/ubuntu/vcadevops/scenarios/springFormSubmission/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job spring_boot_form  --username jenkins --password $PASSWORD  > ./springFormSubmission/jenkins/config.xml
 
 # web_server
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job web_server   --username jenkins --password $PASSWORD > /home/ubuntu/vcadevops/scenarios/webServer/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job web_server   --username jenkins --password $PASSWORD > ./webServer/jenkins/config.xml
 
 # Chef_Knife
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job Chef_Knife   --username jenkins --password $PASSWORD > /home/ubuntu/vcadevops/scenarios/chefKnife/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job Chef_Knife   --username jenkins --password $PASSWORD > ./chefKnife/jenkins/config.xml
 
 # reset_demo
-java -jar /tmp/jenkins-cli.jar -s http://localhost:8100/ get-job reset_demo  --username jenkins --password $PASSWORD > /home/ubuntu/vcadevops/scenarios/resetDemo/jenkins/config.xml
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job reset_demo  --username jenkins --password $PASSWORD > ./resetDemo/jenkins/config.xml
+
+# Word Findfer
+java -jar /tmp/jenkins-cli.jar -s http://vcair.us:8100/ get-job word_finder  --username jenkins --password $PASSWORD > ./wordFinder/jenkins/config.xml
 
