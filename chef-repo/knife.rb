a3f9ad653fe4c30881194c5bb87ca828ffbbb8b7
Bob Webster <rdbwebster@gmail.com>
2015-10-28 16:42:08 -0400

    fixes to webServer

---
+++ chef-repo/knife.rb
@@ -0,0 +1,6 @@
+user = "ubuntu"
+node_name                user
+client_key              "/home/ubuntu/vcadevops/chef-repo/.chef/chef-ubuntu.pem"
+validation_key           "/home/ubuntu/vcadevops/chef-repo/.chef/chef-validator.pem"
+chef_server_url          "https://chef.vcair.us/organizations/chef"
+~                                                                        
